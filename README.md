f4a23d31c9b39c25d6bc47fc8c532476393b9ef2
Alex Zlotnik <zlotnika@gmail.com>
2013-12-30 14:10:04 -0800

    add the content

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ connect-requirements
 ====================
 
 Require specific keys in request.
+
+Usage
+-----
+
+```JavaScript
+var connect = require('connect')
+var requirements = require('connect-requirements')
+
+var app = connect()
+
+  // make sure the objects are added to the request before the request is checked for requirements
+  .use(connect.bodyParser())
+
+  // you can specify any objects within the request to check for keys
+  .use(requirements({ query: 'foo' }))
+  .use(requirements({ body: ['bar', 'foobar'] }))
+
+  .use(function(req, res){
+    res.end('hello world\n');
+  })
+
+http.createServer(app).listen(3000);
+```
---
+++ connect-requirements.js
@@ -0,0 +1,25 @@
+exports = module.exports = function(options) {
+    options = options || {}
+
+    return function requireKeys(request, response, next) {
+
+        for ( var name in options ) {
+            // name as in 'body'
+            var required_keys = options[name]
+            required_keys = Array.isArray(required_keys) ? required_keys : [required_keys]
+
+            for ( var index in required_keys ) {
+                // key as in 'data', or 'language'
+                var key = required_keys[index]
+
+                if ( !request[name].hasOwnProperty(key) ) {
+                    var error_message = 'Error: request missing ' + key + ' in ' + name
+                    response.status(400)
+                    return next(error_message)
+                }
+            }
+        }
+
+        next()
+    }
+}
